370a73181ac12bb655f93b8bb14f908d9cc43b6d
riahk <moar.riah@gmail.com>
2015-01-20 22:16:48 -0800

    implement extend and defaults

--- src/underbar.js
+++ src/underbar.js
@@ -249,11 +249,31 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for(var i = 1; i < arguments.length; i++) {
+      if(arguments[i] != {}) { //checks that it isn't empty
+        var nextObj = arguments[i]
+        for(var j in nextObj) {
+          obj[j] = nextObj[j];
+        }
+      }
+    }
+    return obj;    
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for(var i = 1; i < arguments.length; i++) {
+      if(arguments[i] != {}) { //checks that it isn't empty
+        var nextObj = arguments[i]
+        for(var j in nextObj) {
+          if(obj[j] === undefined) {
+            obj[j] = nextObj[j];
+          }
+        }
+      }
+    }
+    return obj; 
   };
 
 
